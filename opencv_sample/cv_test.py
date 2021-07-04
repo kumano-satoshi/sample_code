1a0b03b7c546cd7aa7c6a0466d47b848c51a1275
Satoshi Kumano <51152864+kumano-satoshi@users.noreply.github.com>
2021-07-05 01:36:33 +0900

    Update cv_test.py

--- opencv_sample/cv_test.py
+++ opencv_sample/cv_test.py
@@ -1,3 +1,5 @@
+#Original_Author -- Koki Shirota
+
 import cv2
 
 img = cv2.imread('lena.png')
@@ -5,5 +7,3 @@ hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 cv2.imshow('img',img)
 cv2.imshow('hsv',hsv)
 cv2.waitKey(0)
-
-#original author -- Koki Shirota
